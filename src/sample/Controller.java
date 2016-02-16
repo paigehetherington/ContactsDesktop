443edaf7e3b0a1adb4fb9580e2ef290306df502e
Paige Hetherington <herbmama@gmail.com>
2016-02-16 12:44:39 -0500

    Initial import

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    ObservableList<Contact> contacts = FXCollections.observableArrayList();
+
+    @FXML
+
+}
